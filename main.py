5371f5c3d239ffb92b2ecb30640e2b70131451e2
DoctorManhattan123 <alexandervonberkov1@gmail.com>
2022-10-11 22:52:33 +0200

    feat: peinlich, 2h stunden für sowas

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ def init_screen():
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     black_square = pygame.image.load('black_square.png')
-    black_square = pygame.transform.scale(black_square, (square_width-1, square_height-1))
+    black_square = pygame.transform.scale(black_square, (square_width - 1, square_height - 1))
 
     green_square = pygame.image.load('green_square.png')
-    green_square = pygame.transform.scale(green_square, (square_width-1, square_height-1))
+    green_square = pygame.transform.scale(green_square, (square_width - 1, square_height - 1))
     screen.fill((255, 255, 255))
 
     running = True
@@ -58,7 +58,8 @@ def init_screen():
         # event handling, gets all event from the event queue
         for y in range(height):
             for x in range(width):
-                screen.blit(green_square if game_board[y][x] == ALIVE else black_square, (square_width * x, square_height * y))
+                screen.blit(green_square if game_board[y][x] == ALIVE else black_square,
+                            (square_width * x, square_height * y))
 
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
@@ -67,7 +68,6 @@ def init_screen():
                 running = False
 
         pygame.time.delay(1000)
-        time.sleep(1)
 
         print(f"round: {round}")
         print_2d_array(game_board)
@@ -84,36 +84,49 @@ def print_2d_array(array2d: List[List[int]]):
         print("\n")
 
 
-def get_num_of_neighbours_by_coords(x, y) -> int:
+def get_num_of_neighbours_by_coords(x_coord, y_coord) -> int:
     """
 
-    :param x: x coord (on the x axis)
-    :param y: y coord (on the y axis)
+    :param x_coord: x coord (on the x-axis)
+    :param y_coord: y coord (on the y-axis)
     :return: the number of neighbours in around the cell with the given coordinates
     """
     global game_board
+    y_begin = y_coord - 1 if y_coord - 1 > -1 else 0
+    y_end = y_coord + 1 if y_coord + 1 < len(game_board) else len(game_board) - 1
+    x_begin = x_coord - 1 if x_coord - 1 > -1 else 0
+    x_end = x_coord + 1 if x_coord + 1 < len(game_board[0]) else len(game_board[0]) - 1
     num = 0
-    for i in range(y-1 if y-1 > -1 else 0, y+1 if y+1 < len(game_board) else len(game_board)-1):
-        for j in range(x-1 if x-1 > -1 else 0, x+1 if x+1 < len(game_board[i]) else len(game_board)-1):
+    for y in range(y_begin, y_end + 1):
+        for x in range(x_begin, x_end + 1):
             # if we are in the center.
-            if i == y and j == x:
+            if y == y_coord and x == x_coord:
                 continue
-            num += 1 if game_board[i][j] == ALIVE else 0
+            num += 1 if game_board[y][x] == ALIVE else 0
     return num
 
 
 def update_board():
     global game_board
-    temp_board = game_board.copy()
+    temp_board = [
+        [ALIVE if game_board[y_coord][x_coord] == ALIVE else DEAD for x_coord in range(len(game_board[y_coord]))] for
+        y_coord in range(len(game_board))]
     for y in range(height):
         for x in range(width):
             num_of_neighbours = get_num_of_neighbours_by_coords(x, y)
-            # any living cell with 2 neighbours survives
-            if temp_board[y][x] == DEAD and num_of_neighbours == 2:
+            # Any live cell with two or three live neighbours survives.
+            if temp_board[y][x] == ALIVE and (num_of_neighbours == 2 or num_of_neighbours == 3):
                 temp_board[y][x] = ALIVE
-            # any cell with 3 neighbours whether alive or dead will become/stay alive
-            elif num_of_neighbours == 3:
+            # Any dead cell with three live neighbours becomes a live cell.
+            elif temp_board[y][x] == DEAD and num_of_neighbours == 3:
                 temp_board[y][x] = ALIVE
+            # All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+            else:
+                temp_board[y][x] = DEAD
+
+    game_board = [
+        [ALIVE if temp_board[y_coord][x_coord] == ALIVE else DEAD for x_coord in range(len(temp_board[y_coord]))] for
+        y_coord in range(len(temp_board))]
 
 
 if __name__ == '__main__':
