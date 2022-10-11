d6be06eccbb38e63e93c9e67f416653b4a98f882
DoctorManhattan123 <alexandervonberkov1@gmail.com>
2022-10-11 22:26:08 +0200

    feat: still doesn't work

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 from typing import List
 
@@ -15,11 +17,18 @@ square_height = screen_height / width
 
 # game_board: List[List[int]] = [[0 if i % 2 == 0 else 1]*width for i in range(height)]
 
-game_board: List[List[int]] = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+game_board: List[List[int]] = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+round = 0
 
 
 def run():
@@ -29,6 +38,8 @@ def run():
 
 def init_screen():
     global game_board
+    global round
+
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
 
@@ -43,11 +54,10 @@ def init_screen():
 
     # main loop
     while running:
+        round += 1
         # event handling, gets all event from the event queue
         for y in range(height):
             for x in range(width):
-                print(f"x: {x}, y: {y}")
-
                 screen.blit(green_square if game_board[y][x] == ALIVE else black_square, (square_width * x, square_height * y))
 
         for event in pygame.event.get():
@@ -56,19 +66,37 @@ def init_screen():
                 # change the value to False, to exit the main loop
                 running = False
 
-        pygame.time.delay(500)
+        pygame.time.delay(1000)
+        time.sleep(1)
+
+        print(f"round: {round}")
+        print_2d_array(game_board)
+
         pygame.display.update()
 
         update_board()
 
 
+def print_2d_array(array2d: List[List[int]]):
+    for y in range(len(array2d)):
+        for x in range(len(array2d[y])):
+            print(f"{array2d[y][x]}, ", end='')
+        print("\n")
+
+
 def get_num_of_neighbours_by_coords(x, y) -> int:
+    """
+
+    :param x: x coord (on the x axis)
+    :param y: y coord (on the y axis)
+    :return: the number of neighbours in around the cell with the given coordinates
+    """
     global game_board
     num = 0
-    for i in [y-1, y, y+1]:
-        for j in range(x-1, x, x+1):
-            # if we are out of bound or in the center we continue
-            if j < 0 or j > len(game_board)-1 or i < 0 or i > len(game_board[j])-1 or (i == x and j == y):
+    for i in range(y-1 if y-1 > -1 else 0, y+1 if y+1 < len(game_board) else len(game_board)-1):
+        for j in range(x-1 if x-1 > -1 else 0, x+1 if x+1 < len(game_board[i]) else len(game_board)-1):
+            # if we are in the center.
+            if i == y and j == x:
                 continue
             num += 1 if game_board[i][j] == ALIVE else 0
     return num
@@ -77,10 +105,8 @@ def get_num_of_neighbours_by_coords(x, y) -> int:
 def update_board():
     global game_board
     temp_board = game_board.copy()
-    # first rule
     for y in range(height):
         for x in range(width):
-            print(f"x: {x}, y: {y}")
             num_of_neighbours = get_num_of_neighbours_by_coords(x, y)
             # any living cell with 2 neighbours survives
             if temp_board[y][x] == DEAD and num_of_neighbours == 2:
